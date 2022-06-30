44aedeea86dc5179ef781864d9105bf6c4d022b4
shootka <pirathaker315@gmail.com>
2022-06-30 22:52:55 +0300

    delete icon, add svg variant of icon, change style

--- client/src/components/Phrase/Phrase.js
+++ client/src/components/Phrase/Phrase.js
@@ -1,53 +1,60 @@
 import React, {useState} from 'react';
 import {useSpeechSynthesis} from "react-speech-kit";
 import AudioReactRecorder, {RecordState} from 'audio-react-recorder'
+import icons from "../../assets/icons.js";
 import './Phrase.scss'
+
 
 const Phrase = ({phrase}) => {
   const {speak, voices} = useSpeechSynthesis();
   const [stop, setStop] = useState(false)
   const [blob, setBlob] = useState()
   const [show, setShow] = useState(false)
-
   const [recordState, setRecordState] = useState(RecordState.NONE)
 
   const startRecording = () => {
     setStop(!stop)
     console.log('start')
     setRecordState(RecordState.START)
-
   }
-
   const stopRecording = () => {
     setStop(!stop)
     console.log('stop')
     setRecordState(RecordState.STOP)
   }
-
   const onStop = (audioData) => {
     console.log('audioData', audioData)
     setBlob(audioData)
   }
-
   return (
     <div className={"phrase-box"}>
-        <div className={'phrase-box__container'}
-             onClick={() => setShow(!show)}>
-          <button onClick={e => e.stopPropagation()}>-</button>
-          <h2 className={show ? "phrase-box__title-active" : "phrase-box__title"}>{phrase}</h2>
-          <div className={"phrase-box__button-box"}
-               onClick={event => (event.stopPropagation())}>
-            <button className="phrase-box__play"
-                    onClick={() => speak({text: phrase, voice: voices[10]})}/>
-            <div style={{display: "none"}}>
-              <AudioReactRecorder type={'audio/mp3'} state={recordState} onStop={onStop}/>
-            </div>
-            <button
-              className={"phrase-box__recorder"}
-              style={{backgroundColor: `${!stop ? 'lawngreen' : '#fc5050'}`}}
-              onClick={() => !stop ? startRecording() : stopRecording()}/>
+
+      <div className={'phrase-box__container'}
+           onClick={() => setShow(!show)}>
+        <button className='btn'
+          onClick={e => e.stopPropagation()}>
+          {icons.DEL}
+        </button>
+        {phrase.length > 20
+          && <h2 className={show
+            ? "phrase-box__title-active bigger"
+            : "phrase-box__title bigger"}>{phrase}</h2>}
+        {phrase.length < 20
+          && <h2 className={"phrase-box__title "}>{phrase}</h2>}
+
+        <div className={"phrase-box__button-box"}
+             onClick={event => (event.stopPropagation())}>
+          <button className="btn"
+                  onClick={() => speak({text: phrase, voice: voices[10]})}>{icons.PLAY}</button>
+          <div style={{display: "none"}}>
+            <AudioReactRecorder type={'audio/mp3'} state={recordState} onStop={onStop}/>
           </div>
+          <button
+            className={"phrase-box__recorder btn"}
+            style={{backgroundColor: `${!stop ? 'lawngreen' : '#fc5050'}`}}
+            onClick={() => !stop ? startRecording() : stopRecording()}>{icons.MICRO}</button>
         </div>
+      </div>
     </div>
   );
 };
--- client/src/components/PhraseList/PhraseList.js
+++ client/src/components/PhraseList/PhraseList.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Phrase from "../Phrase/Phrase";
 import './PhraseList.scss'
-import {useState} from "@types/react";
-
+import Modal from "../Modal/Modal";
+import icons from "../../assets/icons.js";
 const PhraseList = ({phraseList}) => {
   const [modalActive, setModalActive] = useState(false)
+
   return (
     <div className={'container'}>
       <div className={'list'}>
@@ -12,7 +13,10 @@ const PhraseList = ({phraseList}) => {
           return <Phrase key={ph?._id || Math.random()} phrase={ph}/>
         })}
       </div>
-      <button></button>
+      <button className={'add-bnt'} onClick={() => setModalActive(true)}>
+        {icons.ADD}
+      </button>
+      <Modal active={modalActive} setActive={setModalActive}/>
     </div>
   );
 };
