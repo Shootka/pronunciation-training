46834c54cb884be433f8af88c98fb1e6d6cee91d
KostyaBardok <konstantinbardok@gmail.com>
2022-07-06 01:18:22 +0300

    Add cool face AI

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
   app.enableCors()
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(process.env.PORT || 8080);
 }
 
 bootstrap();
